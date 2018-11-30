fba2a17bd02392ab927a316d3d9c0a3f26ba8c2d
topcc <nettopcc123@163.com>
2018-11-30 19:12:45 +0800

    8097

---
+++ src/components/Index8.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div class="mar">
+            <h2 class="tit"><i class="iconfont icon-race"></i>赛程赛果</h2>
+            <vue-banner01d></vue-banner01d>
+            <vue-banner01a></vue-banner01a>
+            <vue-marquee></vue-marquee>
+        </div>
+        <h2 class="tit mt10"><i class="iconfont icon-news02"></i>热门新闻</h2>
+        <vue-newlists8></vue-newlists8>
+    </div>
+</template>
+<script>
+    import marquee from './../components/marquee01';
+    import newlists8 from './../components/newlists9';
+    import banner01a from './../components/banner01_c';
+    import banner01d from './../components/banner01_d';
+    import banner01c from './../components/banner01_c';
+    export default {
+        data() {
+            return {
+                bdTokenUrl: 'https://m.sporttery.cn/app/zf/fb/livelist.html'
+               // bdTokenUrl: 'http://tiyu.baidu.com/home' //https://m.sporttery.cn/app/zf/fb/livelist.html  https://m.sporttery.cn/app/zf/fb/livelist.html http://m.sporttery.cn/wap/dtc/fb_league_europe.html
+            }
+        },
+        // created() {
+        //     this.getUrl();
+        //     this.$nextTick(()=>{
+        //         this.getCode();
+        //     });
+        // },
+        mounted(){
+
+        },
+        components: {
+         'vue-marquee':marquee,
+         'vue-newlists8':newlists8,
+         'vue-banner01a':banner01a,
+         'vue-banner01c':banner01c,
+         'vue-banner01d':banner01d
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "scss/base.scss";
+.child-view{
+    height:100%;
+}
+.mar{
+    overflow: hidden;
+    background: #fff;
+    z-index: 100;
+    position: relative;
+}
+.tit{
+    line-height: 0.36rem;
+    background:#ffffff;
+    color:#272727;
+    text-indent: 0.1rem;
+}
+.tit i{
+    float:left;
+    font-size: 0.2rem;
+}
+.mt10{
+    margin-top:10px;
+}
+</style>
---
+++ src/components/htmlViewSample.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="iftop">
+        <!-- <div class="bghid"></div>     -->
+        <iframe ref="iframe" id="bdIframe" :src="bdTokenUrl" frameborder="0" scrolling="auto" width="100%" height="100%" ></iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import { setTimeout } from 'timers';
+//http://sports.qq.com/kbsweb/kbsshare/gamelist.htm?#nav-laliga
+    export default {
+        data() {
+            return {
+                bdTokenUrl: 'https://m.sporttery.cn/app/zf/fb/livelist.html' //https://m.sporttery.cn/app/zf/fb/livelist.html  https://m.sporttery.cn/app/zf/fb/livelist.html http://www.yidianzixun.com/#/?id=c2&name=%E4%BD%93%E8%82%B2
+            }
+        },
+        // created() {
+        //     this.getUrl();
+        //     this.$nextTick(()=>{
+        //         this.getCode();
+        //     });
+        // },
+        mounted(){
+            this.$nextTick(() => {//在下次 DOM 更新循环结束之后执行延迟回调
+                $('.bghid').on('click',function(){
+                    return;
+                })
+                // setTimeout(function(){
+                //     alert('1aaa')
+                //     $('#bdIframe').attr('height','100000px');
+                //     $('#bdIframe').attr('style','width:100%;height:100000px');
+                //     $('.bghid').attr('style','height:100000px');
+                // },1000)
+            })
+            /**
+             * iframe-宽高自适应显示   
+             */
+            const oIframe = document.getElementById('bdIframe');
+            const deviceWidth = document.documentElement.clientWidth;
+            const deviceHeight = document.documentElement.clientHeight;
+            oIframe.style.width =  '100%'; //数字是页面布局宽度差值
+            oIframe.style.height = '100%'; //数字是页面布局高度差
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import "scss/base.scss";
+.child-view{
+    height:100%;
+}
+.iftop{
+    height: 100%;
+    position: relative
+}
+.bghid{
+    width: 100%;
+    height:100%;
+    background:rgba(255,255,255,0.01);
+    position: absolute;
+    left:0px;
+    top:0px;
+    height: 100%;
+}
+</style>
